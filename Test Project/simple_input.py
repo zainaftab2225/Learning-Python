3002f748b44bb331ae7ebb053b680039688f55c3
Zain Aftab <zainaftab2225@gmail.com>
2020-07-01 09:00:53 +0500

    testing

---
+++ Test Project/simple_input.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+add_num = num1 + num2
+
+
+print("After adding, the numbers are: ", add_num)
--- Test Project/test_module.py
+++ Test Project/test_module.py
@@ -2,5 +2,22 @@ import sys
 print(sys.executable)
 print(sys.version)
 
+'''
 for num in [1, 2, 3, 4, 5]:
     print(num)
+
+print("Hello World")  # Hello World
+print("The answer of 2+2 is: ", 2+2)  # The answer of 2+2 is: 4
+print("The answer of 2+2 is: ", "2+2")  # The answer is: 2+2
+
+print("Hello \" World")
+
+print("The file is stored in C:\rew folder")
+'''
+
+print(2 // 3)
+
+x = 4
+y = 5
+a = ((x) * (y))
+print(a)
